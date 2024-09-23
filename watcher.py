ffa094ef6e9abd9f7c5b88e68389b1da3229f39d
Vaghela divyesh <86907707+iamdivyeshh@users.noreply.github.com>
2024-09-23 18:37:20 +0530

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '7624083076:AAGz7HcmExx7T1l08lyyWYfpcUyzCwd65CY'
+ADMIN_ID = '5294131244'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
